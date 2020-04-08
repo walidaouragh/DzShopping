12ac15fe9c13a1d5616e1fba64efd4dd2883d60d
waouragh <waouragh@activeaero.com>
2020-04-08 17:20:35 -0400

    commit git ignore files

--- DzShopping-UI/src/app/app.component.ts
+++ DzShopping-UI/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'DzShopping-UI';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit() {
+    return this.http.get('https://localhost:5001/api/products').subscribe(p => {
+      console.log('*-*-*-*-*-', p)
+    })
+  }
+
 }
