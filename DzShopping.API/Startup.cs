0f9ebfe952cf19c9427711199e60a56e6dd3df73
waouragh <waouragh@activeaero.com>
2020-04-11 16:56:37 -0400

    Added swagger

--- DzShopping.API/Startup.cs
+++ DzShopping.API/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace DzShopping.API
 {
@@ -34,6 +35,16 @@ namespace DzShopping.API
             services.AddCors();
 
             services.AddScoped<IProductRepository, ProductRepository>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "DzShopping API",
+                    Description = "A simple example ASP.NET Core Web API"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +65,13 @@ namespace DzShopping.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My DzShopping API V1");
+                c.RoutePrefix = "swagger/ui";
             });
         }
     }
