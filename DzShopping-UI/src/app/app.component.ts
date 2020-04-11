e74cbbb2a3701be9de7c1070a70f75ed48e76b37
waouragh <waouragh@activeaero.com>
2020-04-11 15:40:05 -0400

    Added prettier

--- DzShopping-UI/src/app/app.component.ts
+++ DzShopping-UI/src/app/app.component.ts
@@ -2,18 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private http: HttpClient) {
-
-  }
-  ngOnInit() {
-    return this.http.get('https://localhost:5001/api/products').subscribe(p => {
-      console.log('*-*-*-*-*-', p)
-    })
-  }
-
+    constructor(private http: HttpClient) {}
+    ngOnInit() {
+        return this.http.get('https://localhost:5001/api/products').subscribe((p) => {
+            console.log('*-*-*-*-*-', p);
+        });
+    }
 }
