62067626dff4404715c3b5cf8acd16a7ab90374d
waouragh <waouragh@activeaero.com>
2020-05-12 12:04:08 -0400

    Add home page

--- DzShopping-UI/src/app/shared/shared.module.ts
+++ DzShopping-UI/src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { PagerComponent } from './components/pager/pager.component';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 
 @NgModule({
     declarations: [PagerComponent, PagingHeaderComponent],
-    imports: [CommonModule, PaginationModule.forRoot()],
-    exports: [PaginationModule, PagerComponent, PagingHeaderComponent],
+    imports: [CommonModule, CarouselModule.forRoot(), PaginationModule.forRoot()],
+    exports: [PaginationModule, CarouselModule, PagerComponent, PagingHeaderComponent],
 })
 export class SharedModule {}
