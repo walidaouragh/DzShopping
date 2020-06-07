8189d0a1c745ec0df51911e3360cad330facd94b
walidaouragh <34663591+walidaouragh@users.noreply.github.com>
2020-06-07 11:35:52 -0400

    Run prettier

--- DzShopping-UI/src/app/core/not-found-error/not-found-error.component.ts
+++ DzShopping-UI/src/app/core/not-found-error/not-found-error.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-not-found-error',
-  templateUrl: './not-found-error.component.html',
-  styleUrls: ['./not-found-error.component.css']
+    selector: 'app-not-found-error',
+    templateUrl: './not-found-error.component.html',
+    styleUrls: ['./not-found-error.component.css'],
 })
 export class NotFoundErrorComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
